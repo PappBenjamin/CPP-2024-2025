9fa7d54a85cd8b6f390b483111b0380f338dcdb3
Papp Benjámin <pappbenjamin@yahoo.com>
2024-09-24 13:05:07 +0300

    Hibajavítás extra.cpp

--- lab01/extra.cpp
+++ lab01/extra.cpp
@@ -6,16 +6,26 @@ void printNWord(string &fileName, int n)
     map<string, int> bibleWords;
     string word;
 
-
     ifstream bible(fileName);
 
     while(bible >> word)
     {
+        transform(word.begin(), word.end(), word.begin(),::tolower);
+
+        for(int i = 0; i <= word.length(); ++i)
+        {
+            if(!isalpha(word[i])){word.erase(i, 1);}
+        }
         bibleWords[word]++;
     }
 
     vector< pair<string, int> > bibleWordsVector(bibleWords.begin(), bibleWords.end());
     sort(bibleWordsVector.begin(), bibleWordsVector.end(), compare);
+
+    for (int i = 0; i < bibleWordsVector.size(); ++i)
+    {
+        cout << bibleWordsVector[i].first << endl;
+    }
 
 
     cout << "Your " << n << "th common word was: " << bibleWordsVector[n].first << " (" << bibleWordsVector[n].second <<")";
